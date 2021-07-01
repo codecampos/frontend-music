53ed2422a44958c19fc52cea21c6c01702c6689b
Matheus Campos <matheuscp81@gmail.com>
2021-07-01 17:01:01 -0300

    Update index.js

--- src/pages/Carrousel/index.js
+++ src/pages/Carrousel/index.js
@@ -72,7 +72,7 @@ export default function Carrousel() {
           <h3 style={slideThree}>Violões feitos com madeiras nobres, trazendo um som limpo!</h3>
         </div>
         <div>
-          <h3 style={slideFour}>Equipamentos eletronicos de qualidade!</h3>
+          <h3 style={slideFour}>Equipamentos eletrônicos de qualidade!</h3>
         </div>
       </Carousel>
     </>
